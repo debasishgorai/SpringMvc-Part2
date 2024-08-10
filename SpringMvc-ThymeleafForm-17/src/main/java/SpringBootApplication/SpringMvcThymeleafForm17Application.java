149ad21bd6b0e6db520e23909250d914b6a91618
Debasish Gorai <119739769+debasishgorai@users.noreply.github.com>
2024-08-10 23:27:37 +0530

    Add files via upload

---
+++ SpringMvc-ThymeleafForm-17/src/main/java/SpringBootApplication/SpringMvcThymeleafForm17Application.java
@@ -0,0 +1,13 @@
+package SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcThymeleafForm17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcThymeleafForm17Application.class, args);
+	}
+
+}
---
+++ SpringMvc-ThymeleafForm-17/src/test/java/SpringBootApplication/SpringMvcThymeleafForm17ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcThymeleafForm17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
